cf301433c3046a836365d25426f93dcfc7c377b3
mfoltz <mitchell.foltz@gmail.com>
2024-04-26 17:59:05 -0500

    sync

--- obj/Debug/net6.0/Cobalt.AssemblyInfo.cs
+++ obj/Debug/net6.0/Cobalt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cobalt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0582f3cd654bf8739d0090d6f9ede4739a780a51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+798fb9e7af4bb7ae9f0a6582fdb985a249d99e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cobalt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cobalt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
