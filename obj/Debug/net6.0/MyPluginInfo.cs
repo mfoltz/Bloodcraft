7d62ec4c6cc0f40e690d99067974f0870dde94fd
zfolmt <mitchell.foltz@gmail.com>
2024-05-17 16:04:05 -0500

    Delete obj directory

--- obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Cobalt
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Cobalt";
-        public const string PLUGIN_NAME = "Cobalt";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
