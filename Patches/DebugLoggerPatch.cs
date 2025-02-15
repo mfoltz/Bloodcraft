cdf9aab58169291fe654a8ddfd6b562a9fd8dcfa
mfoltz <mitchell.foltz@gmail.com>
2025-02-14 21:09:26 -0600

    - github doesnt like the type for map icon error patch, sigh

---
+++ Patches/DebugLoggerPatch.cs
@@ -0,0 +1,30 @@
+﻿using Bloodcraft.Services;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Bloodcraft.Patches;
+
+/*
+[HarmonyPatch]
+internal class DebugLoggerPatch
+{
+    static readonly bool _familiars = ConfigService.FamiliarSystem;
+    const string MAP_ICON_ERROR = "PlayerMapIcon requires the creator to have the PlayerCharacter component.";
+
+    [HarmonyPatch(typeof(Debug), nameof(Debug.LogError), [typeof(Il2CppSystem.Object)])]
+    [HarmonyPrefix]
+    static bool LogErrorPrefix(Il2CppSystem.Object message)
+    {
+        if (!Core._initialized) return true;
+        else if (!_familiars) return true;
+
+        string stringMessage = message.ToString();
+        if (stringMessage.Contains(MAP_ICON_ERROR))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+*/
