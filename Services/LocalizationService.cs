114c16899ffe4b6bc140ce8cb13d59b0d75bdc07
zfolmt <mitchell.foltz@gmail.com>
2025-08-11 19:36:34 -0500

    Add Brazilian messages path and translations

--- Services/LocalizationService.cs
+++ Services/LocalizationService.cs
@@ -103,7 +103,8 @@ internal class LocalizationService // the bones are from KindredCommands, ty Odj
         {"German", "Bloodcraft.Resources.Localization.Messages.German.json"},
         {"French", "Bloodcraft.Resources.Localization.Messages.French.json"},
         {"Spanish", "Bloodcraft.Resources.Localization.Messages.Spanish.json"},
-        {"Italian", "Bloodcraft.Resources.Localization.Messages.Italian.json"}
+        {"Italian", "Bloodcraft.Resources.Localization.Messages.Italian.json"},
+        {"Brazilian", "Bloodcraft.Resources.Localization.Messages.Brazilian.json"}
     };
 
     static readonly Dictionary<uint, string> _messageDictionary = [];
