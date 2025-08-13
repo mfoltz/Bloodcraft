0996d8f9e7dbea6deeca72dc64822d4898ea5448
zfolmt <mitchell.foltz@gmail.com>
2025-08-13 13:25:05 -0500

    Use localized keys for emote messages

---
+++ Resources/Localization/MessageKeys.cs
@@ -0,0 +1,18 @@
+namespace Bloodcraft.Resources.Localization;
+
+internal static class MessageKeys
+{
+    public const string EMOTE_ACTIONS_DOMINATE_FORM = "You can't use emote actions when using dominate form!";
+    public const string EMOTE_ACTIONS_BAT_FORM = "You can't use emote actions when using bat form!";
+    public const string FAMILIAR_INTERACT_COMBAT = "You can't interact with your familiar during combat!";
+    public const string SHAPESHIFT_SELECT_FORM = "Select a form you've unlocked first! ('<color=white>.prestige sf [<color=orange>EvolvedVampire|CorruptedSerpent</color>]</color>')";
+    public const string FAMILIAR_CALL_PVP_COMBAT = "You can't call your familiar during PvP combat!";
+    public const string FAMILIAR_ACTIVE_NOT_EXIST = "Active familiar doesn't exist! If that doesn't seem right try using '<color=white>.fam reset</color>'.";
+    public const string FAMILIAR_NOT_FOUND = "Couldn't find active familiar...";
+    public const string FAMILIAR_COMBAT_NOT_ENABLED = "Familiar combat is not enabled.";
+    public const string FAMILIAR_COMBAT_TOGGLE_IN_COMBAT = "You can't toggle familiar combat mode during PvE/PvP combat!";
+    public const string FAMILIAR_COMBAT_ENABLED = "Familiar combat <color=green>enabled</color>.";
+    public const string FAMILIAR_COMBAT_DISABLED = "Familiar combat <color=red>disabled</color>.";
+    public const string FAMILIAR_INTERACT_DISMISSED = "Can't interact with familiar when dismissed!";
+    public const string FAMILIAR_INTERACT_BINDING = "Can't interact with familiar when binding/unbinding!";
+}
