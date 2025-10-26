0f25b409755362f80ce041212805931930af4ded
zfolmt <mitchell.foltz@gmail.com>
2025-10-25 21:35:41 -0500

    Add injectable callbacks and on-update tests for factory works

--- .codex/tests/Systems/Factory/QuestTargetWork.cs
+++ .codex/tests/Systems/Factory/QuestTargetWork.cs
@@ -10,6 +10,24 @@ namespace Bloodcraft.Tests.Systems.Factory;
 public sealed class QuestTargetWork : ISystemWork
 {
     /// <summary>
+    /// Delegate invoked when a target entity should be added or updated in the cache.
+    /// </summary>
+    /// <param name="entity">Entity handle representing the quest target.</param>
+    public delegate void TargetCacheUpdater(EntityHandle entity);
+
+    /// <summary>
+    /// Delegate invoked when a target entity should be tracked as blacklisted.
+    /// </summary>
+    /// <param name="entity">Entity handle representing the blacklisted target.</param>
+    public delegate void BlacklistUpdater(EntityHandle entity);
+
+    /// <summary>
+    /// Delegate invoked when an imprisoned unit should be tracked.
+    /// </summary>
+    /// <param name="entity">Entity handle representing the imprisoned unit.</param>
+    public delegate void ImprisonedUpdater(EntityHandle entity);
+
+    /// <summary>
     /// Initial capacity assigned to the target unit cache.
     /// </summary>
     public const int TargetUnitsCapacity = 1024;
@@ -56,6 +74,42 @@ public sealed class QuestTargetWork : ISystemWork
 
     static readonly QueryDescription targetQuery = CreateTargetQuery();
     static readonly QueryDescription imprisonedQuery = CreateImprisonedQuery();
+
+    readonly TargetCacheUpdater? targetUpdater;
+    readonly BlacklistUpdater? blacklistUpdater;
+    readonly ImprisonedUpdater? imprisonedUpdater;
+    readonly Func<EntityHandle, bool>? targetFilter;
+    readonly Func<EntityHandle, bool>? blacklistEvaluator;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuestTargetWork"/> class.
+    /// </summary>
+    public QuestTargetWork()
+        : this(null, null, null, null, null)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="QuestTargetWork"/> class.
+    /// </summary>
+    /// <param name="targetUpdater">Callback used when caching viable targets.</param>
+    /// <param name="blacklistUpdater">Callback used when caching blacklisted targets.</param>
+    /// <param name="imprisonedUpdater">Callback used when caching imprisoned targets.</param>
+    /// <param name="targetFilter">Predicate controlling which targets are processed.</param>
+    /// <param name="blacklistEvaluator">Predicate controlling which targets are marked as blacklisted.</param>
+    public QuestTargetWork(
+        TargetCacheUpdater? targetUpdater,
+        BlacklistUpdater? blacklistUpdater,
+        ImprisonedUpdater? imprisonedUpdater,
+        Func<EntityHandle, bool>? targetFilter,
+        Func<EntityHandle, bool>? blacklistEvaluator)
+    {
+        this.targetUpdater = targetUpdater;
+        this.blacklistUpdater = blacklistUpdater;
+        this.imprisonedUpdater = imprisonedUpdater;
+        this.targetFilter = targetFilter;
+        this.blacklistEvaluator = blacklistEvaluator;
+    }
 
     /// <summary>
     /// Gets the primary target query description used by the work.
@@ -105,5 +159,33 @@ public sealed class QuestTargetWork : ISystemWork
     /// <inheritdoc />
     public void OnUpdate(SystemContext context)
     {
+        if (targetUpdater != null || blacklistUpdater != null || targetFilter != null || blacklistEvaluator != null)
+        {
+            context.ForEachEntity(targetQuery, ProcessTarget);
+        }
+
+        if (imprisonedUpdater != null)
+        {
+            context.ForEachEntity(imprisonedQuery, entity => imprisonedUpdater?.Invoke(entity));
+        }
+    }
+
+    void ProcessTarget(EntityHandle entity)
+    {
+        if (targetFilter != null && !targetFilter(entity))
+        {
+            return;
+        }
+
+        targetUpdater?.Invoke(entity);
+
+        if (blacklistUpdater != null)
+        {
+            var isBlacklisted = blacklistEvaluator?.Invoke(entity) ?? false;
+            if (isBlacklisted)
+            {
+                blacklistUpdater(entity);
+            }
+        }
     }
 }
